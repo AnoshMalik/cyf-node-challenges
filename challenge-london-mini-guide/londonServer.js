bddc70e53e7c54cf2e1fae274cf96482b889f8af
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-10 21:30:28 +0000

    Server Level 200 - working

--- challenge-london-mini-guide/londonServer.js
+++ challenge-london-mini-guide/londonServer.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const app = express();
 const port = 3030;
-const stratfordPharmacies = require("./data/Stratford.json");
+const stratfordObject = require("./data/Stratford.json");
 
 // 
 app.get("/", (request, response) => {
-    response.send("Returning some value");
+    response.send("Returning no specific value. CHANGE ROUTE");
 
 });
-
-
 
 // PHARMACIES DIRECTORY
-app.get("/stratford", (request, response) => {
-  response.send(stratfordPharmacies);
+app.get("/pharmacies", (request, response) => {
+  response.send(returnPharmacies(stratfordObject));
 });
+
+app.get("/colleges", (request, response) => {
+  response.send(returnColleges(stratfordObject));
+});
+
+app.get("/doctors", (request, response) => {
+  response.send(returnDoctors(stratfordObject));
+});
+
+app.get("/hospitals", (request, response) => {
+  response.send(returnHospitals(stratfordObject));
+});
+
 
 app.listen(port, () => {
     console.log("App is listening on PORT:3030");
@@ -23,7 +34,22 @@ app.listen(port, () => {
 
 
 // Pharmacies function
-function returnPharmacies() { 
+function returnPharmacies(object) {
+    return object["pharmacies"];
+}
 
 
+// Colleges function
+function returnColleges(object) {
+    return object["colleges"];
+}
+
+// Doctors function
+function returnDoctors(object) {
+    return object["doctors"];
+}
+
+// hospital function
+function returnHospitals(object) {
+    return object["hospitals"];
 }
