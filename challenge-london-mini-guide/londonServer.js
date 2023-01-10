8e06f1b414a08788fcb50c76755f6c9d905eea5c
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-10 21:03:12 +0000

    Test - Returning stratford pharmacies

--- challenge-london-mini-guide/londonServer.js
+++ challenge-london-mini-guide/londonServer.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3030;
+const stratfordPharmacies = require("./data/Stratford.json");
 
-
+// 
 app.get("/", (request, response) => {
     response.send("Returning some value");
 
 });
 
 
+
+// PHARMACIES DIRECTORY
+app.get("/stratford", (request, response) => {
+  response.send(stratfordPharmacies);
+});
+
 app.listen(port, () => {
     console.log("App is listening on PORT:3030");
 
 });
+
+
+// Pharmacies function
+function returnPharmacies() { 
+
+
+}
